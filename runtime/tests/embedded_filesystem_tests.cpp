3679fa66956d4cf0421031027836979372859f8e
Danilo Benetti <benetti.danilo@icloud.com>
2026-01-11 16:25:43 +0100

    added unit tests for embedded filesystem

---
+++ runtime/tests/embedded_filesystem_tests.cpp
@@ -0,0 +1,40 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <Ultralight/String.h>
+
+#include "embedded_filesystem.hpp"
+
+TEST_CASE("EmbeddedFileSystem normalizes paths for lookups") {
+    auto fs = CreateEmbeddedFileSystem();
+    REQUIRE(fs);
+
+    REQUIRE(fs->FileExists(ultralight::String("index.html")));
+    REQUIRE(fs->FileExists(ultralight::String("./index.html")));
+    REQUIRE(fs->FileExists(ultralight::String(".\\index.html")));
+    REQUIRE(fs->FileExists(ultralight::String("file:///index.html")));
+
+    REQUIRE_FALSE(fs->FileExists(ultralight::String("missing.txt")));
+    REQUIRE_FALSE(fs->FileExists(ultralight::String("")));
+}
+
+TEST_CASE("EmbeddedFileSystem exposes MIME types and charset") {
+    auto fs = CreateEmbeddedFileSystem();
+    REQUIRE(fs);
+
+    REQUIRE(fs->GetFileMimeType(ultralight::String("index.html")) == "text/html");
+    REQUIRE(fs->GetFileMimeType(ultralight::String("index.css")) == "text/css");
+    REQUIRE(fs->GetFileMimeType(ultralight::String("missing.bin")) == "application/octet-stream");
+    REQUIRE(fs->GetFileMimeType(ultralight::String("")) == "application/octet-stream");
+    REQUIRE(fs->GetFileCharset(ultralight::String("index.html")) == "utf-8");
+}
+
+TEST_CASE("EmbeddedFileSystem opens embedded assets") {
+    auto fs = CreateEmbeddedFileSystem();
+    REQUIRE(fs);
+
+    auto buffer = fs->OpenFile(ultralight::String("index.html"));
+    REQUIRE(buffer);
+
+    auto missing = fs->OpenFile(ultralight::String("missing.txt"));
+    REQUIRE_FALSE(missing);
+}
